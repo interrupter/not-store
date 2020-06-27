c00c17f46db4ff357466e558112d2b7a504952a0
Aleksander Repin <alex.repin@mail.ru>
2020-06-27 18:53:09 +0300

    sidemenu fix

--- src/controllers/root/index.js
+++ src/controllers/root/index.js
@@ -1,3 +1,22 @@
 import ncStore from './ncStore.js';
 
+let manifest = {
+	router: {
+		manifest: [
+			{
+				paths: ['store\/([^\/]+)\/([^\/]+)', 'store\/([^\/]+)', 'store'],
+				controller: ncStore
+			}
+		]
+	},
+	menu:[{
+		title: 	'Файлы',
+		url: 		'/store',
+    items: [{
+  		title: 	'Загрузить',
+  		url: 		'/store/create'
+  	}]
+	}],
+};
+
 export {ncStore, manifest};

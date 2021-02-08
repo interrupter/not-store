60b51b27810cad9a2bc5180e70baa52946559a95
Aleksander Repin <alex.repin@mail.ru>
2021-02-08 12:24:34 +0300

    ncFile fixes labels, title, table model factory

--- src/controllers/root/ncFile.js
+++ src/controllers/root/ncFile.js
@@ -1,80 +1,100 @@
 import Validators from '../common/validators.js';
 
 import {
-	ncCRUD
+  ncCRUD
 } from 'not-bulma';
 
+const MODULE_NAME = '';
+const MODEL_NAME = 'file';
+const ERROR_DEFAULT = 'Что-то пошло не так!';
+
 const LABELS = {
-	plural: 'Ключи',
-	single: 'Ключ',
+  plural: 'Файлы',
+  single: 'Файл',
 };
 
 class ncFile extends ncCRUD {
-	constructor(app, params) {
-		super(app);
-		this.setModuleName('file');
-		this.setOptions('names', LABELS);
-		this.setOptions('Validators', Validators);
-		this.setOptions('params', params);
-		this.setOptions('preload', {});
-		this.setOptions('list', {
-			fields: [{
-				path: ':fileID',
-				title: 'ID',
-				searchable: true,
-				sortable: true
-			}, {
-				path: ':name',
-				title: 'Имя',
-				searchable: true,
-				sortable: true
-			}, {
-				path: ':extension',
-				title: 'Формат',
-				searchable: true,
-				sortable: true
-			}, {
-				path: ':bucket',
-				title: 'Бакет',
-				searchable: true,
-				sortable: true
-			}, {
-				path: ':path.micro.cloud.Location',
-				title: 'Превью',
-				searchable: true,
-				sortable: true,
-				type: 'image',
-			}, {
-				path: ':_id',
-				title: 'Действия',
-				type: 'button',
-				preprocessor: (value) => {
-					return [{
-						action: this.goDetails.bind(this, value),
-						title: 'Подробнее',
-						size: 'small'
-					},{
-						action: this.goUpdate.bind(this, value),
-						title: 'Изменить',
-						size: 'small'
-					},{
-						action: this.goDelete.bind(this, value),
-						type: 'danger',
-						title: 'Удалить',
-						size: 'small',
-						style: 'outlined'
-					}];
-				},
-			}]
-		});
-		this.start();
-		return this;
-	}
+  constructor(app, params) {
+    super(app, `${MODEL_NAME}`);
+    this.setModuleName(MODULE_NAME);
+    this.setModelName(MODEL_NAME);
+    this.setOptions('names', LABELS);
+    this.setOptions('Validators', Validators);
+    this.setOptions('params', params);
+    this.setOptions('list', {
+			interface: {
+				factory: 				this.getModel(),
+				combined: 			true,
+				combinedAction: 'listAndCount'
+			},
+			pager: {
+				size: 100,
+				page: 0
+			},
+			sorter:{
+				fileID: -1
+			},
+      fields: [{
+        path: ':fileID',
+        title: 'ID',
+        searchable: true,
+        sortable: true
+      }, {
+        path: ':name',
+        title: 'Имя',
+        searchable: true,
+        sortable: true
+      }, {
+        path: ':extension',
+        title: 'Формат',
+        searchable: true,
+        sortable: true
+      }, {
+        path: ':bucket',
+        title: 'Бакет',
+        searchable: true,
+        sortable: true
+      }, {
+        path: ':path.micro.cloud.Location',
+        title: 'Превью',
+        searchable: true,
+        sortable: true,
+        type: 'image',
+      }, {
+        path: ':_id',
+        title: 'Действия',
+        type: 'button',
+        preprocessor: (value) => {
+          return [{
+            action: this.goDetails.bind(this, value),
+            title: 'Подробнее',
+            size: 'small'
+          }, {
+            action: this.goUpdate.bind(this, value),
+            title: 'Изменить',
+            size: 'small'
+          }, {
+            action: this.goDelete.bind(this, value),
+            type: 'danger',
+            title: 'Удалить',
+            size: 'small',
+            style: 'outlined'
+          }];
+        },
+      }]
+    });
+    this.start();
+    return this;
+  }
 
-	createDefault() {
-		return {
+  createDefault() {
+    return {
 
-		};
+    };
+  }
+
+  getItemTitle(item){
+		return item.fileID + '#' + item.name;
 	}
 }
 

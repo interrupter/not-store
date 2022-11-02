6857b0b27cf942e8b271807dad3dbd0b87a62f4b
interrupter <alex.repin@mail.ru>
2022-11-03 01:23:34 +0300

    fix: http/https replaced by fetch API

--- index.js
+++ index.js
@@ -1,48 +1,68 @@
-const path = require('path');
-const {notError} = require('not-error');
-const Log = require('not-log')(module, 'init');
-const notStore = require('./src/common/store');
-const notStoreImage = require('./src/common/storeImage');
-const notStoreAWS = require('./src/common/storeAWS');
-const notStoreYandex = require('./src/common/storeYandex');
+const path = require("path");
+const { notError } = require("not-error");
+const Log = require("not-log")(module, "init");
+const notStore = require("./src/common/store");
+const notStoreImage = require("./src/common/storeImage");
+const notStoreAWS = require("./src/common/storeAWS");
+const notStoreYandex = require("./src/common/storeYandex");
 
 module.exports = {
-  notStore,
-  notStoreImage,
-  notStoreAWS,
-  notStoreYandex,
-  name: 'not-store',
-	paths: {
-		routes:				path.join(__dirname, '/src/routes'),
-    controllers:	path.join(__dirname, '/src/controllers'),
-    logics:	      path.join(__dirname, '/src/logics'),
-		models:				path.join(__dirname, '/src/models'),
-    fields:       path.join(__dirname, '/src/fields'),
-	},
-  initialize: (notApp)=>{
-    let Store = notApp.getModel('Store');
-    Store.listAndCount(0, 100, {}, { active: true, __latest: true, __closed:false }, null)
-      .then((results)=>{
-        Log.log(`Found ${results.count} stores options`);
-        if(results.count > 0){
-          Log.log(`Stores init start`);
-          for(let storeOpts of results.list){
-            Log.log(`Adding ${storeOpts.name} via ${storeOpts.driver}`);
-            try{
-              let options = JSON.parse(storeOpts.options);
-              let store = new module.exports[storeOpts.driver](options);
-            	notStore.addInterface(storeOpts.name, store);
-            }catch(e){
-              Log.error(`notStore storage initialization failed; ${storeOpts.name} via ${storeOpts.driver}`);
-              notApp.report(new notError('notStore storage initialization failed', { store: storeOpts }, e));
-            }
-          }
-        }
-        Log.log(`Stores init end`);
-      })
-      .catch((e)=>{
-        Log.error(`notStore initialization failed`);
-        notApp.report(new notError('notStore initialization failed', {}, e));
-      });
-  }
+    notStore,
+    notStoreImage,
+    notStoreAWS,
+    notStoreYandex,
+    name: "not-store",
+    paths: {
+        routes: path.join(__dirname, "/src/routes"),
+        controllers: path.join(__dirname, "/src/controllers"),
+        logics: path.join(__dirname, "/src/logics"),
+        models: path.join(__dirname, "/src/models"),
+        fields: path.join(__dirname, "/src/fields"),
+    },
+    initialize: (notApp) => {
+        let Store = notApp.getModel("Store");
+        Store.listAndCount(
+            0,
+            100,
+            {},
+            { active: true, __latest: true, __closed: false },
+            null
+        )
+            .then((results) => {
+                Log.log(`Found ${results.count} stores options`);
+                if (results.count > 0) {
+                    Log.log(`Stores init start`);
+                    for (let storeOpts of results.list) {
+                        Log.log(
+                            `Adding ${storeOpts.name} via ${storeOpts.driver}`
+                        );
+                        try {
+                            let options = JSON.parse(storeOpts.options);
+                            let store = new module.exports[storeOpts.driver](
+                                options
+                            );
+                            notStore.addInterface(storeOpts.name, store);
+                        } catch (e) {
+                            Log.error(
+                                `notStore storage initialization failed; ${storeOpts.name} via ${storeOpts.driver}`
+                            );
+                            notApp.report(
+                                new notError(
+                                    "notStore storage initialization failed",
+                                    { store: storeOpts },
+                                    e
+                                )
+                            );
+                        }
+                    }
+                }
+                Log.log(`Stores init end`);
+            })
+            .catch((e) => {
+                Log.error(`notStore initialization failed`);
+                notApp.report(
+                    new notError("notStore initialization failed", {}, e)
+                );
+            });
+    },
 };
--- src/common/store.js
+++ src/common/store.js
@@ -1,93 +1,103 @@
-const FIRST_DIR_NAME_LENGTH = 2;
-const uuidv4      = require('uuid').v4;
+const FIRST_DIR_NAME_LENGTH = 3;
+const uuidv4 = require("uuid").v4;
 
 const interfaces = {};
 
 function interfaceExists(interfaceName) {
-	return Object.prototype.hasOwnProperty.call(interfaces, interfaceName);
+    return Object.prototype.hasOwnProperty.call(interfaces, interfaceName);
 }
 
-function getInterface(interfaceName){
-	return interfaces[interfaceName];
+function getInterface(interfaceName) {
+    return interfaces[interfaceName];
 }
 
-function removeInterface(interfaceName){
-	delete interfaces[interfaceName];
+function removeInterface(interfaceName) {
+    delete interfaces[interfaceName];
 }
 
 const getAllMethods = (obj) => {
-	let props = [];
-	do {
-		const l = Object.getOwnPropertyNames(obj)
-			.concat(Object.getOwnPropertySymbols(obj).map(s => s.toString()))
-			.sort()
-			.filter((p, i, arr) =>
-				typeof obj[p] === 'function' && //only the methods
-				p !== 'constructor' && //not the constructor
-				(i == 0 || p !== arr[i - 1]) && //not overriding in this prototype
-				props.indexOf(p) === -1 //not overridden in a child
-			);
-		props = props.concat(l);
-	}
-	while (
-		(obj = Object.getPrototypeOf(obj)) && //walk-up the prototype chain
-		Object.getPrototypeOf(obj) //not the the Object prototype methods (hasOwnProperty, etc...)
-	);
-	return props;
+    let props = [];
+    do {
+        const l = Object.getOwnPropertyNames(obj)
+            .concat(Object.getOwnPropertySymbols(obj).map((s) => s.toString()))
+            .sort()
+            .filter(
+                (p, i, arr) =>
+                    typeof obj[p] === "function" && //only the methods
+                    p !== "constructor" && //not the constructor
+                    (i == 0 || p !== arr[i - 1]) && //not overriding in this prototype
+                    props.indexOf(p) === -1 //not overridden in a child
+            );
+        props = props.concat(l);
+    } while (
+        (obj = Object.getPrototypeOf(obj)) && //walk-up the prototype chain
+        Object.getPrototypeOf(obj) //not the the Object prototype methods (hasOwnProperty, etc...)
+    );
+    return props;
 };
 
 function interfaceMethodExists(interfaceName, methodName) {
-	return getAllMethods(interfaces[interfaceName]).indexOf(methodName) > -1;
+    return getAllMethods(interfaces[interfaceName]).indexOf(methodName) > -1;
 }
 
 function createStandartCall(methodName) {
-	return (interfaceName, ...useful)=>{
-		if (interfaceExists(interfaceName) && interfaceMethodExists(interfaceName, methodName)) {
-			return getInterface(interfaceName)[methodName](...useful);
-		}
-	};
+    return (interfaceName, ...useful) => {
+        if (
+            interfaceExists(interfaceName) &&
+            interfaceMethodExists(interfaceName, methodName)
+        ) {
+            return getInterface(interfaceName)[methodName](...useful);
+        }
+    };
 }
 
-
 function extendFromInterface(storeInterface) {
-	for (let i of getAllMethods(storeInterface)) {
-		if (!Object.prototype.hasOwnProperty.call(exports, i)) {
-			//eslint-disable-next-line no-console
-			//console.log('Add new methods for interface forwarding', i);
-			exports[i] = createStandartCall(i);
-		}
-	}
+    for (let i of getAllMethods(storeInterface)) {
+        if (!Object.prototype.hasOwnProperty.call(exports, i)) {
+            //eslint-disable-next-line no-console
+            //console.log('Add new methods for interface forwarding', i);
+            exports[i] = createStandartCall(i);
+        }
+    }
 }
 
 exports.addInterface = (name, storeInterface) => {
-	if (interfaceExists(name)) {
-		return false;
-	} else {
-		interfaces[name] = storeInterface;
-		extendFromInterface(storeInterface);
-		return true;
-	}
+    if (interfaceExists(name)) {
+        return false;
+    } else {
+        interfaces[name] = storeInterface;
+        extendFromInterface(storeInterface);
+        return true;
+    }
 };
 
 exports.getInterface = (name) => {
-	if (interfaceExists(name)) {
-		return getInterface(name);
-	} else {
-		return false;
-	}
+    if (interfaceExists(name)) {
+        return getInterface(name);
+    } else {
+        return false;
+    }
 };
 
-exports.removeInterface =  (name) => {
-	if (interfaceExists(name)) {
-		removeInterface(name);
-	}
-	return true;
+exports.removeInterface = (name) => {
+    if (interfaceExists(name)) {
+        removeInterface(name);
+    }
+    return true;
 };
 
 exports.getPath = (uuid) => {
-	return uuid.substr(0, FIRST_DIR_NAME_LENGTH);
+    return uuid.substr(0, FIRST_DIR_NAME_LENGTH);
 };
 
 exports.createFileName = () => {
-	return uuidv4();
+    return uuidv4();
+};
+
+exports.getPathFromHash = function (hash) {
+    return (
+        hash.substr(0, FIRST_DIR_NAME_LENGTH) +
+        "/" +
+        hash.substr(FIRST_DIR_NAME_LENGTH)
+    );
 };

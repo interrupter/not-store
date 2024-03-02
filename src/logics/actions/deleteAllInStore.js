4b54bb01c59b73c6137f7d9509c3c795701f7721
interrupter <alex.repin@mail.ru>
2024-03-02 20:01:55 +0300

    added: logic File.deleteAllInStore

--- src/logics/actions/deleteAllInStore.js
+++ src/logics/actions/deleteAllInStore.js
@@ -1,40 +1,36 @@
-const getApp = require('not-node/src/getApp');
-const { LogicDeleteActionException } = require("not-node/src/exceptions/action");
+const getApp = require("not-node/src/getApp");
+const {
+    LogicDeleteActionException,
+} = require("not-node/src/exceptions/action");
 
 module.exports = class deleteAllInStoreAction {
-
-    static checkIdentitySessionId(identity){
-        return typeof identity.sid !== "undefined" &&
-                identity.sid !== null &&
-                identity.sid &&
-                identity.sid.length > 10;
+    static checkIdentitySessionId(identity) {
+        return (
+            typeof identity.sid !== "undefined" &&
+            identity.sid !== null &&
+            identity.sid &&
+            identity.sid.length > 10
+        );
     }
 
     static async run(logic, actionName, { identity, storeName }) {
         try {
             logic.logDebugAction(actionName, identity);
             const File = getApp().getModel("File");
-            if (identity.admin) {
-                await File.getOneByIdAndRemove(targetId, undefined);                
-            } else if (this.checkIdentitySessionId(identity)) {
-                await File.getOneByIdAndRemove(targetId, identity.sid);                
-            } else {
-                throw new Error("no user identity");
-            }
+            await File.deleteAllInStore(storeName, {}, false);
         } catch (e) {
             throw new LogicDeleteActionException(
                 {
-                    activeUserId: identity?.uid,
-                    sessionId: identity?.sid,
-                    role: identity?.role,
                     actionName,
-                    targetId,
+                    activeUserId: identity?.uid,
                     admin: identity?.admin,
+                    role: identity?.role,
                     root: identity?.root,
+                    sessionId: identity?.sid,
+                    storeName,
                 },
                 e
             );
         }
     }
-    
 };

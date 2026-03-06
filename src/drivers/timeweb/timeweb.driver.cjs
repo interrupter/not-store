b2df64aa0d1fe9f43012fc875f04ed6a8674dada
Aleksandr Repin <admin@appmon.ru>
2026-03-06 15:44:44 +0300

    fix: file extension for parent upload

--- src/drivers/timeweb/timeweb.driver.cjs
+++ src/drivers/timeweb/timeweb.driver.cjs
@@ -186,10 +186,10 @@ class notStoreDriverTimeweb extends notStoreDriver {
                     uuid: this.uuid(),
                     name_tmp: fileInfo.prepared.local
                 };
-                fullFileName = fileInfo.prepared.fullFilenameInStore;
+                
             }else{
                 fileIds = await this.stashFile(file);
-                fullFileName = this.composeFullFilename(fileIds.uuid, undefined, fileInfo.format);
+                
             }
             const { name_tmp, uuid } = fileIds;            
             tmpFilename = name_tmp;
@@ -207,6 +207,11 @@ class notStoreDriverTimeweb extends notStoreDriver {
                 },
                 this
             );
+            if(fileInfo.prepared ){
+                fullFileName = fileInfo.prepared.fullFilenameInStore;
+            }else{
+                fullFileName = this.composeFullFilename(fileIds.uuid, undefined, fileInfo.format || fileInfo?.metadata?.format);
+            }
             const result = await this.directUpload(
                 name_tmp,
                 fullFileName
